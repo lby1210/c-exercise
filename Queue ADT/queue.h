4457cc4669116e67e931d95f641370fcb230373d
lby1210 <563191705@qq.com>
2017-06-12 22:25:08 +0800

    queue
    队列ADT练习

---
+++ Queue ADT/queue.h
@@ -0,0 +1,33 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+#include <stdbool.h>
+#define MAXQUEUE 10
+
+	typedef int Item;
+	
+	typedef struct node{
+		Item item;
+		struct node* next;
+	}Node;
+	
+	typedef struct queue{
+		Node* front;
+		Node* rear;
+		int items;
+	}Queue;
+	
+	void InitializeQueue(Queue *pq);
+	
+	bool QueueIsEmpty(const Queue *pq);
+	
+	bool QueueIsFull(const Queue *pq);
+	
+	int QueueItemCount(const Queue*pq);
+	
+	bool EnQueue(Item item,Queue*pq);
+	
+	bool DeQueue(Item *pitem,Queue *pq);
+	
+	void EmptyTheQueue(Queue*pq);
+	
+#endif 
